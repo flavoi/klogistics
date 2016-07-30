cefefd4b35438ccc588abbadf55f406d33091df3
flavoi <flavius476@gmail.com>
2016-07-30 17:53:34 +0200

    Added season context.

---
+++ klogistics/seasons/context.py
@@ -0,0 +1,10 @@
+"""
+    Custom context processors for the seasons app.
+    This script contains useful informations for every template.
+"""
+from .models import Season
+
+def get_seasons(request):
+    """ Restituisce l'elenco delle stagioni disponibili. """
+    seasons = Season.objects.get_available_seasons()
+    return { 'seasons': seasons }
