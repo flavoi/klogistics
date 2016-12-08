c30f04ec503b012d79259e44cba787168352622d
flavoi <flavius476@gmail.com>
2016-12-08 16:31:48 +0100

    Deactivated debug.

--- klogistics/config/settings/production.py
+++ klogistics/config/settings/production.py
@@ -10,7 +10,7 @@ def get_env_variable(var_name):
         error_msg = "Set the {0} enviroment variable".format(var_name)
         raise ImproperlyConfigured(error_msg)
 
-DEBUG = True
+DEBUG = False
 
 SECRET_KEY = get_env_variable('SECRET_KEY')
 
